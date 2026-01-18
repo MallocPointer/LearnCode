6a290105be5ad5bbb91e3f0711320fdf9e3499d2
MallocPointer <lizhongran910@gmail.com>
2026-01-18 15:24:36 +0800

    手机按键

---
+++ luogu/P1765.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	//只算了26个字母 空格单独算
+	int cnt[26] = {
+		1,2,3, 1,2,3, 1,2,3,
+		1,2,3, 1,2,3,
+		1,2,3,4,
+		1,2,3,
+		1,2,3,4
+	};
+
+	string s;
+	getline(cin, s);
+
+	int ans = 0;
+	for (char c : s) {
+		if (c == ' ')   //单独算空格
+			ans += 1;
+		else
+			ans += cnt[c - 'a'];
+	}
+
+	cout << ans;
+//	system("pause");
+	return 0;
+}
