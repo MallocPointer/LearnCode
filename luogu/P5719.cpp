8063d500b44d87ea6161eb8906532853ca48184c
MallocPointer <lizhongran910@gmail.com>
2026-01-18 13:32:58 +0800

    分类平均

---
+++ luogu/P5719.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(void) {
+	int n, k;
+	cin >> n >> k;
+	int yes = 0;
+	int no = 0;
+	int cnt = 0;   //专记录yes
+
+	for (int i = 1; i <= n; i++) {
+		if (i % k == 0) {
+			yes += i;
+			cnt++;
+		}
+		else no += i;
+	}
+
+	cout << fixed << setprecision(1)
+		<< (double)yes / cnt << " "
+		<< (double)no / (n - cnt) << endl;
+
+	system("pause");
+	return 0;
+}
