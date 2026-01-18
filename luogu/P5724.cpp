c2f7c845eec62d8bf9f2efd14d86679cdb304968
MallocPointer <lizhongran910@gmail.com>
2026-01-18 16:39:36 +0800

    排序

---
+++ luogu/P5724.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	vector<int> num;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int data;
+		cin >> data;
+		num.push_back(data);
+	}
+
+	sort(num.begin(), num.end());
+
+	cout << num[n - 1] - num[0] << endl;
+	
+//	system("pause");
+	return 0;
+}
