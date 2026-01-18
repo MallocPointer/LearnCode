0641c39a478ed306aa2c4ecb2df6e1c541ba19ad
MallocPointer <lizhongran910@gmail.com>
2026-01-18 17:09:07 +0800

    去掉最值

---
+++ luogu/P5738.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	double maxScore = 0.0;   //最大值初始化为最低
+
+	for (int i = 0; i < n; i++) {
+		vector<int> scores(m);
+		int sum = 0;
+		int mx = 0, mn = 10;
+
+		for (int j = 0; j < m; j++) {
+			cin >> scores[j];
+			sum += scores[j];
+			mx = max(mx, scores[j]);
+			mn = min(mn, scores[j]);
+		}
+
+		double avg = (sum - mx - mn) * 1.0 / (m - 2);
+		maxScore = max(maxScore, avg);
+	}
+
+	cout << fixed << setprecision(2) << maxScore << endl;
+
+//	system("pause");
+	return 0;
+}
