87cc564dcb4fba7b4dc6038782683f9b745f8a13
MallocPointer <lizhongran910@gmail.com>
2026-01-18 16:32:05 +0800

    小数部分省略操作

---
+++ luogu/P5720.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void) {
+	int a;
+	cin >> a;
+	int cnt = 1;
+	while (a != 1) {
+		a /= 2;
+		cnt++;
+	}
+	cout << cnt << endl;
+//	system("pause");
+	return 0;
+}
