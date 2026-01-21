65618f79dc748cbda0a1d6dd13e80674ed7a1c97
MallocPointer <lizhongran910@gmail.com>
2026-01-21 11:54:04 +0800

    连续数找最小

---
+++ luogu/P1614.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> arr;
+	for (int i = 0; i < n; i++) {
+		int tem;
+		cin >> tem;
+		arr.push_back(tem);
+	}
+
+	//特殊情况 m == 0
+	if (m == 0) {
+		cout << 0 << endl;
+		return 0;
+	}
+
+	vector<int> fin;
+
+	for (int i = 0; i <= n - m; i++) {
+		int sum = 0;
+		for (int j = 0; j < m; j++) {
+			sum += arr[i + j];
+		}
+		fin.push_back(sum);
+	}
+
+	// 输出最小值
+	cout << *min_element(fin.begin(), fin.end()) << endl;
+
+//	system("pause");
+	return 0;
+}
