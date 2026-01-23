0126853a6f5dffad4d5f39b337c32a0d3322dc8b
MallocPointer <lizhongran910@gmail.com>
2026-01-23 11:50:07 +0800

    反向输出

---
+++ luogu/P1427.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	vector<int> arr;
+	int num;
+
+	// 记录数值
+	while (cin >> num) {
+		if (num == 0) break;
+		arr.push_back(num);
+	}
+
+	for (int i = (int)arr.size() - 1; i >= 0; --i) {
+		if (i != (int)arr.size() - 1) cout << ' ';
+		cout << arr[i];
+	}
+
+	system("pause");
+	return 0;
+}
