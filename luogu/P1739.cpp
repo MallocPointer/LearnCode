f8e8b19917648c03f7fc244c17d6cf9860a74fb7
MallocPointer <lizhongran910@gmail.com>
2026-01-26 22:14:46 +0800

    栈检查有里到外是否存在对称符号

---
+++ luogu/P1739.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main() {
+	stack<char> st;
+	vector<char> comp;
+	string s;
+
+	cin >> s;
+
+	for (char ch : s) {
+		if (ch == '@') break;
+		comp.push_back(ch);
+	}
+
+	for (char ch : comp) { 
+		if (ch == '(') {
+			st.push(ch);
+		}
+		else if (ch == ')') {
+			if (st.empty()) {
+				cout << "NO" << endl;
+				return 0;
+			}
+			st.pop();
+		}
+	}
+
+	if (st.empty())
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+//	system("pause");
+	return 0;
+}
