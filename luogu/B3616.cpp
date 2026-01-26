e42572ffe9582a684eed317c4f8f6cc45e9660a8
MallocPointer <lizhongran910@gmail.com>
2026-01-27 00:12:16 +0800

    队列简单应用2

---
+++ luogu/B3616.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+	int n;   // n组数据
+	cin >> n;
+
+	queue<int> q;
+
+	while (n--) {
+		int op;
+		cin >> op;
+
+		switch(op) {
+		case 1:
+			int num;
+			cin >> num;
+			q.push(num);
+			break;
+		case 2:
+			if (q.empty()) {
+				cout << "ERR_CANNOT_POP" << endl;
+			}
+			else {
+				q.pop();
+			}
+			break;
+		case 3:
+			if (q.empty()) {
+				cout << "ERR_CANNOT_QUERY" << endl;
+			}
+			else cout << q.front() << endl;
+			break;
+		case 4:
+			cout << q.size() << endl;
+			break;
+		default:
+			break;
+		}
+	}
+
+
+//	system("pause");
+	return 0;
+}
