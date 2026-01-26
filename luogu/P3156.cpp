56c6c2526e58b1f10034ebc55b699488fc16c480
MallocPointer <lizhongran910@gmail.com>
2026-01-26 10:43:46 +0800

    索引查找

---
+++ luogu/P3156.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> a(n + 1);  // 1-based
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	while (m--) {
+		int k;
+		cin >> k;
+		cout << a[k] << endl;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int now;
+		cin >> now;
+		cout << a[now] << endl;
+	}
+
+	//	system("pause");
+	return 0;
+}
