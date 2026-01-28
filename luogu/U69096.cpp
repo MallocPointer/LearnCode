85c81e1d71d1b8bad488d7cdca27f3f35acbfdfa
MallocPointer <lizhongran910@gmail.com>
2026-01-28 23:59:17 +0800

    前缀和

---
+++ luogu/U69096.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	vector<int> arr;
+	
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+
+	vector<int> original;
+	for (int i = 0; i < n; i++) {
+		if (i == 0) {
+			original.push_back(arr[0]);  // 0号索引前缀和就是自己
+		}
+		else {
+			original.push_back(arr[i] - arr[i - 1]);
+		}
+		cout << original[i] << " ";
+	}
+
+
+//	system("pause");
+	return 0;
+}
