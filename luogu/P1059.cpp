159c9418d2d3fd128335efd72b332ddf4319d109
MallocPointer <lizhongran910@gmail.com>
2026-02-02 20:25:27 +0800

    set去重以及迭代器遍历

---
+++ luogu/P1059.cpp
@@ -0,0 +1,26 @@
+﻿//https://www.luogu.com.cn/training/946266#problems
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	set<int> arr;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		arr.insert(num);
+	}
+
+	cout << arr.size() << endl;
+
+	for (auto it = arr.begin(); it != arr.end(); ++it) {
+		cout << *it << " ";
+	}
+
+//	system("pause");
+	return 0;
+}
