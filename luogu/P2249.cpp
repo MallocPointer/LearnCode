89881218fbaf3d6e368edbb922761c01e60c3475
MallocPointer <lizhongran910@gmail.com>
2026-02-02 22:14:27 +0800

    二分查找数字编号

---
+++ luogu/P2249.cpp
@@ -0,0 +1,38 @@
+﻿//https://www.luogu.com.cn/problem/P2249
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> arr;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+
+//  sort(arr.begin(), arr.end());
+	for (int i = 0; i < m; i++) {
+		int which;
+		cin >> which;
+
+		auto it = lower_bound(arr.begin(), arr.end(), which);
+		int place;
+		if (it != arr.end() && *it == which) place = (int)(it - arr.begin()) + 1; // 题目下标从 1 开始
+		else place = -1;
+
+		cout << place << " ";
+	}
+
+//	system("pause");
+	return 0;
+}
