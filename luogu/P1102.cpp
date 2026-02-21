93542271fe6db2e90cea52d24d09fdb7ee685032
MallocPointer <lizhongran910@gmail.com>
2026-02-21 16:40:24 +0800

    A - B 要考虑相同数字

---
+++ luogu/P1102.cpp
@@ -0,0 +1,38 @@
+﻿// https://www.luogu.com.cn/problem/P1102
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+using llong = long long;
+
+int main() {
+	int N;
+	llong C;
+	cin >> N >> C;
+
+	vector<llong> arr(N);
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr.begin(), arr.end());
+
+	llong ans = 0;
+
+	for (int i = 0; i < N; i++) {
+		llong target = arr[i] + C;
+
+		auto left = lower_bound(arr.begin(), arr.end(), target);
+		auto right = upper_bound(arr.begin(), arr.end(), target);
+
+		ans += (right - left);
+	}
+
+	cout << ans << endl;
+
+//	system("pause");
+	return 0;
+}
